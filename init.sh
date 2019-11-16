a4ccceb86e3f16a5e7076513746d95f2ed32afd1
Gabriel Escamilla <gabriel@escamillas.com>
2019-11-16 15:38:21 -0500

    building for ubuntu

--- init.sh
+++ init.sh
@@ -1,4 +1,31 @@
-#!/bin/bash
-
+#!/bin/zsh
 git pull || { echo 'Failed to pull, stopping here' && exit 1 ; }
 git submodule update --init --recursive
+cd
+
+source ./install_ubuntu.sh
+
+for file in .zshrc .zpreztorc; do
+	if [[ ! -a "$HOME/$file" && ! -h "$HOME/$file" ]]; then
+		ln -s ~/.home/zsh/$file ~/$file
+	fi
+done
+
+for file in .tmux.conf .tmux .gitconfig .vim .vimrc .iterm-settings .ignore; do
+	if [[ ! -a "$HOME/$file" && ! -h "$HOME/$file" ]]; then
+		ln -s ~/.home/$file ~/$file
+	fi
+done
+
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.home/zsh/prezto/runcoms/^README.md(.N); do
+	if [ ! -s "${ZDOTDIR:-$HOME}/.${rcfile:t}" ]; then
+		ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+	fi
+done
+
+type rg &>/dev/null || { echo 'rg not installed!' ; }
+type fd &>/dev/null || { echo 'fd not installed!' ; }
+type fzf &>/dev/null || { echo 'fzf not installed!' ; }
+type tmux &>/dev/null || { echo 'tmux not installed!' ; }
+type pydf &>/dev/null || { echo 'pydf not installed!' ; }
