368cefe9f4f16fd75f0067b4034ab52b87973ee1
Gabriel Escamilla <chivygab@gmail.com>
2020-05-12 23:35:15 +0000

    WIP

--- init.sh
+++ init.sh
@@ -1,6 +1,13 @@
-#/bin/bash
+#!/bin/bash
 git pull || { echo 'Failed to pull, stopping here' && exit 1; }
 git submodule update --init --recursive
 
 cd
+
+if [ $(uname -a | grep -c "Ubuntu") -eq 1 ];
+then
+  . ~/.home/os/ubuntu_setup.sh
+fi
+
+# dot files links
 ln -s ~/.home/vim/vimrc ~/.vimrc
