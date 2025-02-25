a79cf21aa8a9fc29c740a26993912f693907d662
gabrielescamilla <chivygab@gmail.com>
2025-02-25 14:51:42 -0500

    move to setup script

--- setup.zsh
+++ setup.zsh
@@ -3,6 +3,14 @@
 # This line sets the variable 'filename' to the first argument passed to the script
 filename=$1
 
+# Ripgrep and completion
+brew install ripgrep
+brew install fzf
+
+# Intall neovim
+brew install 
+ln -s ${HOME}/.home/neovim/config ${HOME}/.config/nvim
+
 # This line prints the hostname of the current system
 echo "Hostname: $(hostname)"
 
